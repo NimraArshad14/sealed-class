97f9b22d98b1661e30047e2129f1f2f8a447d38e
Nimramayo <manimra840@gmail.com>
2024-10-12 16:47:08 +0500

    seales-class

--- src/Main.kt
+++ src/Main.kt
@@ -7,6 +7,14 @@ fun main() {
     println(evaluate(addition))
     println(evaluate(subtraction))
     println(evaluate(multiplication))
+
+    println("==================")
+
+    val rectangle = rectangle(width = 4.5 , height = 5.6)
+    val circle = circle(5.0)
+
+    println(calculatearea(rectangle))
+    println(calculatearea(circle))
 }
 sealed  class operation {
 
@@ -21,4 +29,14 @@ fun evaluate(op : operation) : Int {
          is operation.multiply -> 5 * op.value
      }
 
+}
+sealed interface shape
+class rectangle(val width : Double , val height : Double) : shape
+class circle (val radius : Double) : shape
+
+fun calculatearea(shape:shape) : Double {
+    return when(shape) {
+        is rectangle -> shape.width * shape.height
+        is circle -> shape.radius * shape.radius
+    }
 }
